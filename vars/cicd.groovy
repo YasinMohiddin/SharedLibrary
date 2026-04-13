f26afb8507ade65fe480d66c94131232e8169fee
Shaik Yasin Mohiddin <165322270+YasinMohiddin@users.noreply.github.com>
2026-04-13 16:04:42 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -10,3 +10,7 @@ def gitDeployment(jobname,ip,context)
 {
   sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war"
 }
+def runSelenium(jobname)
+{
+  "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+}
